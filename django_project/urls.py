0e274819f7903a776101137f0b0aa0f90b6174ac
kairat-kempirbaev <7647704+kairat-kempirbaev@users.noreply.github.com>
2024-07-12 22:01:56 -0700

    Fix #2 in URLS.py

--- django_project/urls.py
+++ django_project/urls.py
@@ -30,6 +30,6 @@ urlpatterns = [
     path("admin-blog/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
     path("blog/", include(wagtail_urls)),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
